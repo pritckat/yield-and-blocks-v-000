948af875173870ace20e3bab29afc21bbe075b81
Katrina Pritchard <pritckat@gmail.com>
2018-08-14 22:31:01 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -10,7 +10,7 @@ end
 # call your method here!
 
 hello_t(["Tim", "Tom", "Jim"]) { |name|
-if name.starts_with("T")
-  puts "Hi, #{name}"
-end
+  if name.starts_with("T")
+    puts "Hi, #{name}"
+  end
 }
